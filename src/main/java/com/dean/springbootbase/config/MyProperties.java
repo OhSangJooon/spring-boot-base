6cae4bdd7f660c64a1d3096b137181f24e87cb06
OhSangJooon <dhdldl1@naver.com>
2024-04-01 21:42:13 +0900

    study#1-base

--- src/main/java/com/dean/springbootbase/config/MyProperties.java
+++ src/main/java/com/dean/springbootbase/config/MyProperties.java
@@ -4,16 +4,15 @@ import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+@Getter
 @ConstructorBinding
 @ConfigurationProperties("my")
 public class MyProperties {
+    /* 제 키에요 */
     private final Integer height;
 
     public MyProperties(Integer height) {
         this.height = height;
     }
 
-    public Integer getHeight() {
-        return height;
-    }
 }
